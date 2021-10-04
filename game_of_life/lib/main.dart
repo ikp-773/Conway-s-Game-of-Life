9de0952a1214c6221dd5359003b5ec0e004f0546
Abhijith Kp <abhijithkp773@gmail.com>
2021-10-05 01:13:01 +0530

    Update main.dart

--- game_of_life/lib/main.dart
+++ game_of_life/lib/main.dart
@@ -3,11 +3,18 @@ import 'package:game_of_life/screens/initial_life.dart';
 import 'package:get/get.dart';
 
 void main() {
-  runApp(
-    GetMaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       home: InitialLife(),
       theme: ThemeData.dark(),
-    ),
-  );
+    );
+  }
 }
